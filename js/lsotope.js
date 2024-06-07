2c9c1718af1b3944fec7bbbec5c019f9ee5998ce
Piumi Dissanayake <dissanayakepiumi75@gmail.com>
2024-06-07 23:18:45 +0530

    add data filter

--- js/lsotope.js
+++ js/lsotope.js
@@ -15,3 +15,43 @@ const select = (el, all = false) => {
  * Eassy event listener function 
  */
 
+const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all);
+    if(selectEl) {
+        if (all) {
+            selectEl.forEach(e => e.addEventListener(type, listener));
+        }else{
+            selectEl.addEventListener(type, listener);
+        }
+    }
+};
+
+/**
+ * Portfolio isotope and filter
+ */
+window.addEventListener('load', () => {
+    let portfolioContainer = select('.portfolio-container');
+    if (portfolioContainer){
+        let portfolioIsotope = new Isotope(portfolioContainer,{
+            itemSelector:'.portfolio-item',
+        });
+        let portfoliofilters = select('#portfolio-filters li',true);
+
+        on(
+            'click',
+            '#portfolio-filters li',
+            function (e) {
+                e.preventDefault();
+                portfoliofilters.forEach(function(el){
+                    el.classList.remove('filter-active');
+
+                });
+                this.classList.add('filter-active');
+
+                portfolioIsotope.arrange({
+                    filter: this.getAttribute('data-filter'),
+                })
+            }
+        );
+    }
+});
